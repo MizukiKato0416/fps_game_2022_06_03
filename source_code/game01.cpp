c3d674ab3d0b8946a0af403f12dc927a75d662bf
mimimimimi0416 <201014@st.yoshida-g.ac.jp>
2022-06-03 14:42:47 +0900

    Update

--- source_code/game01.cpp
+++ source_code/game01.cpp
@@ -50,7 +50,8 @@ HRESULT CGame01::Init(void)
 	//メッシュフィールド生成
 	CMeshField *pMeshField = CMeshField::CreateLoadText("data/mesh_field.txt");
 	pMeshField->BindTexture(CManager::GetInstance()->GetTexture()->GetTexture("grass001.png"));
-	
+	CFloor *pFloor = CFloor::Create(D3DXVECTOR3(0.0f, 60.0f, 0.0f), D3DXVECTOR3(500.0f, 000.0f, 500.0f), D3DXVECTOR3(0.2f, 0.0f, 0.0f));
+	pFloor->BindTexture(CManager::GetInstance()->GetTexture()->GetTexture("grass001.png"));
 
 	return S_OK;
 }
