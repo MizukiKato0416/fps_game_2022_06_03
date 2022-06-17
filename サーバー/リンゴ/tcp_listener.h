275bc3849d0fee87f0127553f6975bbfce2603d3
HatoriTaichi <201113@st.yoshida-g.ac.jp>
2022-06-17 16:30:34 +0900

    サーバーの更新

--- サーバー/リンゴ/tcp_listener.h
+++ サーバー/リンゴ/tcp_listener.h
@@ -24,8 +24,15 @@ public:
 	bool Init(void);	// 初期化
 	CCommunication *Accept(void);	// 接続待ち
 	void Uninit(void);	// 終了
+	bool GetIsInit(void) { return m_is_init; }	// 初期化しているかの判定の取得
+	bool GetIsUninit(void) { return m_is_uninit; }	// 終了しているかの判定の取得
+	int GetNumber(void) { return m_my_number; }	// 自分の部屋番号の取得
+	void SetNumber(int number) { m_my_number = number; }	// 自分の部屋番号の設定
 private:
 	SOCKET m_sockServer;	// ソケット
+	int m_my_number;	//　自分の部屋番号
+	bool m_is_init;	// 初期化しているか
+	bool m_is_uninit;	// 終了しているか
 };
 
 #endif // _TCP_CLIENT_H_
