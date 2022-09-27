f562656d0438a272a9870e56b8f442d42b1f07b1
mimimimimi0416 <201014@st.yoshida-g.ac.jp>
2022-09-27 12:48:45 +0900

    last

--- source_code/sound.cpp
+++ source_code/sound.cpp
@@ -183,18 +183,18 @@ HRESULT CSound::Init(void)
 
 	//‰¹—Ê’²®
 	//m_pSubmixVoice->SetVolume(0.1f);
-	ControllVoice(SOUND_LABEL::GAME_BGM,			0.6f);
-	ControllVoice(SOUND_LABEL::GAME_LAST_SPURT_BGM, 0.6f);
-	ControllVoice(SOUND_LABEL::RESULT_BGM,			0.8f);
-	ControllVoice(SOUND_LABEL::TITLE_BGM,			1.6f);
-	ControllVoice(SOUND_LABEL::RELOAD_SE,			8.0f);
-	ControllVoice(SOUND_LABEL::RELOAD_FIN_SE,		5.0f);
-	ControllVoice(SOUND_LABEL::HIT_SE,				3.0f);
-	ControllVoice(SOUND_LABEL::KILL_SE,				2.0f);
-	ControllVoice(SOUND_LABEL::GAME_OVER_SE,		0.6f);
-	ControllVoice(SOUND_LABEL::SHOT_SE,				4.0f);
-	ControllVoice(SOUND_LABEL::START_SHOT_SE,		0.2f);
-	ControllVoice(SOUND_LABEL::TITLE_WIND_SE,		1.0f);
+	ControllVoice(SOUND_LABEL::GAME_BGM,			0.6f * 1.4f);
+	ControllVoice(SOUND_LABEL::GAME_LAST_SPURT_BGM, 0.6f * 1.4f);
+	ControllVoice(SOUND_LABEL::RESULT_BGM,			0.8f * 1.4f);
+	ControllVoice(SOUND_LABEL::TITLE_BGM,			1.6f * 1.4f);
+	ControllVoice(SOUND_LABEL::RELOAD_SE,			8.0f * 1.4f);
+	ControllVoice(SOUND_LABEL::RELOAD_FIN_SE,		5.0f * 1.4f);
+	ControllVoice(SOUND_LABEL::HIT_SE,				3.0f * 1.4f);
+	ControllVoice(SOUND_LABEL::KILL_SE,				2.0f * 1.4f);
+	ControllVoice(SOUND_LABEL::GAME_OVER_SE,		0.6f * 1.4f);
+	ControllVoice(SOUND_LABEL::SHOT_SE,				4.0f * 1.4f);
+	ControllVoice(SOUND_LABEL::START_SHOT_SE,		0.2f * 1.4f);
+	ControllVoice(SOUND_LABEL::TITLE_WIND_SE,		1.0f * 1.4f);
 /*	
 	// Detailis‚ÌŽæ“¾
 	XAUDIO2_VOICE_DETAILS Detailis;
